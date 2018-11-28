71d3f122c0ef92ef72ba930e3344f90280cfcc76
GianinaZarate <monseerrath.16@gmail.com>
2018-11-27 23:57:03 -0300

    Update Traductor_tarea3.c

--- Traductor_tarea3.c
+++ Traductor_tarea3.c
@@ -3,3 +3,22 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#define CANT_COMP_LEX 10
+FILE *archivo_salida;
+void json_trad();
+void element_trad();
+void array_trad();
+void arrayB_trad();
+void element_list_trad();
+void element_listB_trad();
+void object_trad();
+void objectB_trad();
+void attribute_trad();
+void attributes_list_trad();
+void attributes_listB_trad();
+void attribute_name_trad();
+void attribute_value_trad();
+void match(int);
+void string_sin_comillas(char* cadena);
+int tabulacion=0;
+void print_tab();
